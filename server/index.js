9821815b8f25d8d29fe2e89d5f3a5463205af42e
Léa Buendé <lea.buende.tchiendjou@efrei.net>
2020-11-08 09:55:02 +0100

    update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 const index_port = 3000
-<<<<<<< HEAD
 const ingredients = require('../server/routes/ingredients')
 var cors = require('cors')
 
@@ -22,33 +21,4 @@ app.use('/ingredients/', ingredients)
 
 
 app.listen(index_port, () => console.log(`Example app listening on port ${index_port}!`))
-=======
-
-app.get('/', (req, res) => res.send('Hello World!'))
-app.post('/', (req, res) => res.send('Hello POST World!'))
-
-//Jusque là, tout va bien
-
-app.listen(index_port, () => console.log(`Example app listening on port ${index_port}!`))
-const mysql = require('mysql');
-
-const host = process.env.DB_HOST || '138.68.98.48';
-const port = process.env.DB_PORT || '3306';
-const user = process.env.DB_USER || 'root';
-const password = process.env.DB_PASS || 'root';
-const database = process.env.DB_DATABASE || 'groak';
-
-const con = mysql.createConnection({
-  host, port, user, password, database,
-});
-const query = "SELECT * FROM ingredients";
- 
-con.connect(function(err) {
-  if (err) throw err;
-  con.query(query, (err, result, fields) => {
-    if (err) throw err;
-    console.log(result);
- });
-});
->>>>>>> a40b8b1356ec9ba938ff375f87d4f150898af683
 
